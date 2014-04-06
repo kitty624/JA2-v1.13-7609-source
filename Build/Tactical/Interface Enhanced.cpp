94d18c21cf2df2f221566b8b1cf2f2dbf1d8075a
Wanne <Wanne@3b4a5df2-a311-0410-b5c6-a8a6f20db521>
2014-04-06 18:31:40 +0000

    Bugfix (by anv) - possible crash when viewing general stats of machineguns with no single shot available (due to comparison feature)
    git-svn-id: https://ja2svn.mooo.com/source/ja2/branches/ja2_source_official_2014@7140 3b4a5df2-a311-0410-b5c6-a8a6f20db521

--- Build/Tactical/Interface Enhanced.cpp
+++ Build/Tactical/Interface Enhanced.cpp
@@ -8293,11 +8293,10 @@ void DrawWeaponValues( OBJECTTYPE * gpItemDescObject )
 			// Reset font color
 			SetFontForeground( 6 );
 		}
-		else
+		else if( fComparisonMode )
 		{
 			if( !Weapon[gpComparedItemDescObject->usItem].NoSemiAuto )
-			{
-				INT16 iComparedSingleAPCost = BaseAPsToShootOrStab( APBPConstants[DEFAULT_APS], APBPConstants[DEFAULT_AIMSKILL], gpComparedItemDescObject, NULL );
+			{				INT16 iComparedSingleAPCost = BaseAPsToShootOrStab( APBPConstants[DEFAULT_APS], APBPConstants[DEFAULT_AIMSKILL], gpComparedItemDescObject, NULL );
 				SetFontForeground( ITEMDESC_FONTPOSITIVE );
 				swprintf( pStr, L"%d", iComparedSingleAPCost );
 			}
