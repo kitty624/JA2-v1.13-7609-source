9905122ff5d72f372a1f3bb8ea73a64411855300
Wanne <Wanne@3b4a5df2-a311-0410-b5c6-a8a6f20db521>
2014-05-02 13:40:02 +0000

    Fixes (by Buggler) - Show map gridno with hotkey 'F' in release build as well for more informative map-related bug reports - Fixed skipping of bloodcat battles using hotkey 'Esc' in prebattle interface when battle initiated at strategic screen
    git-svn-id: https://ja2svn.mooo.com/source/ja2/branches/ja2_source_official_2014@7182 3b4a5df2-a311-0410-b5c6-a8a6f20db521

--- Build/Tactical/Turn Based Input.cpp
+++ Build/Tactical/Turn Based Input.cpp
@@ -3229,11 +3229,9 @@ void GetKeyboardInput( UINT32 *puiNewEvent )
 							DisplayRangeToTarget( MercPtrs[ gusSelectedSoldier ], usGridNo );
 						}
 
-						#ifdef JA2TESTVERSION
-							CHAR16	zOutputString[512];
-							swprintf( zOutputString, L"gridno: %d", usGridNo);
-							ScreenMsg( FONT_MCOLOR_LTYELLOW, MSG_INTERFACE, zOutputString );
-						#endif
+						CHAR16	zOutputString[512];
+						swprintf( zOutputString, L"%s: %d", pUpdateMapInfoText[19], usGridNo);
+						ScreenMsg( FONT_MCOLOR_LTYELLOW, MSG_INTERFACE, zOutputString );
 					}
 				}
 				break;
@@ -3242,7 +3240,7 @@ void GetKeyboardInput( UINT32 *puiNewEvent )
 				if( fCtrl && UsingNewInventorySystem() == true )
 				{
 					HandleTBPickUpBackpacks();
-						}
+				}
 				SeperateItems();
 				if( fCtrl )
 					StackAndSort( TRUE );
@@ -3304,7 +3302,7 @@ void GetKeyboardInput( UINT32 *puiNewEvent )
 				if( fCtrl )
 				{
 					HandleTBToggleFormation();
-					}
+				}
 				else if ( gGameSettings.fOptions[TOPTION_GL_BURST_CURSOR] )
 				{
 					gGameSettings.fOptions[TOPTION_GL_BURST_CURSOR] = FALSE;
